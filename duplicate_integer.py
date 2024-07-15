e35cb826907931cec83626534f108dc075f0c593
Rice <garmen.lai727@gmail.com>
2024-07-15 20:10:26 +1000

    finished implementing hasDuplicate function

---
+++ duplicate_integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        number_set = set()
+
+        for num in nums:
+            if num not in number_set:
+                number_set.add(num)
+            else:
+                return True
+
+        return False
